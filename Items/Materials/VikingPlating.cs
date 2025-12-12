a8886939c1236ee3f459004028acb613d8f7382a
Andrew Wang <115036225+drew-gnaw@users.noreply.github.com>
2025-12-12 00:02:39 -0800

    Add initial implementation for viking armor

---
+++ Items/Materials/VikingPlating.cs
@@ -0,0 +1,24 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace DasherClass.Items.Materials
+{
+    public class VikingPlating : ModItem, ILocalizedModType
+    {
+        public new string LocalizationCategory => "Items.Materials";
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 5;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 20;
+            Item.height = 20;
+            Item.maxStack = 9999;
+            Item.value = Item.sellPrice(silver: 1);
+            Item.rare = ItemRarityID.Green;
+        }
+    }
+}
