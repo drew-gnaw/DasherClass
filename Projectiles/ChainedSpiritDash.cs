2244de12754878f31261c1d23f04dfa4daf51de5
ChonkyChonker <louisjyluo@gmail.com>
2025-12-15 00:08:34 -0800

    remove unneeded dependencies

--- Projectiles/ChainedSpiritDash.cs
+++ Projectiles/ChainedSpiritDash.cs
@@ -1,10 +1,8 @@
-using DasherClass.Items.Weapons;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Terraria;
 using Terraria.ModLoader;
-using Terraria.ID;
-using System;
+
 
 namespace DasherClass.Projectiles
 {
